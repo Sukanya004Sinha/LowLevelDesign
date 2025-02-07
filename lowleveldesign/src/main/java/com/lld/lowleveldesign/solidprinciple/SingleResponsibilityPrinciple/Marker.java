917cd6316eb99a9f8aac132869be3b0e23d53096
sukanya004sinha <sukanyasinha004@gmail.com>
2025-02-07 18:27:11 +0530

    upto modifiers

---
+++ lowleveldesign/src/main/java/com/lld/lowleveldesign/solidprinciple/SingleResponsibilityPrinciple/Marker.java
@@ -0,0 +1,11 @@
+package com.lld.lowleveldesign.solidprinciple.SingleResponsibilityPrinciple;
+public class Marker {
+    String name;
+    String color;
+    int year;
+    int price;
+    public Marker(String name, String color, int year, int price){
+
+    }
+
+}
---
+++ lowleveldesign/src/main/java/oops/javasourcefileintroduction/importstatement/Test.java
@@ -0,0 +1,12 @@
+package oops.javasourcefileintroduction.importstatement;
+
+public class Test {
+    public static void main(String[] args){
+        String s = new String ("Durga");
+        Thread t = new Thread();
+        Exception e = new Exception();
+        StringBuffer sb = new StringBuffer("Sukanya");
+        Student s1 = new Student();
+        Student.m1();
+    }
+}
