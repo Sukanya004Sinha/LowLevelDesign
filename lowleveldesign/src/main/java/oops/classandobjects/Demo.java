23019464c724dc91aa30f4310b32acb0fe8f2bfd
sukanya004sinha <sukanyasinha004@gmail.com>
2025-02-10 22:53:11 +0530

    Upto Method overloading

--- lowleveldesign/src/main/java/oops/classandobjects/Demo.java
+++ lowleveldesign/src/main/java/oops/classandobjects/Demo.java
@@ -1,16 +1,29 @@
 package oops.classandobjects;
+
+/**
+ * Method Overloading - Same number of paraneter but differnt types or different numbers of parameter but same types
+ */
 class Calculators{
-    public int add1 (int n1, int n2){
+    public int add (int n1, int n2){
         int sum = n1+n2;
         return sum;
     }
+    public int add (int n1, int n2, int n3){
+        int sum = n1+n2+n3;
+        return sum;
+    }
+    public double add (double n1, double n2, double n3){
 
+        return  n1+n2+n3;
+    }
 }
 public class Demo {
     public static void main(String[] args){
         Calculators obj = new Calculators();
-        int test = obj.add1(3,4);
+        int test = obj.add(3,4, 5);
+        double test1 = obj.add(3.0, 4.5, 9.0);
         System.out.println(test);
+        System.out.println(test1);
     }
 
 }
